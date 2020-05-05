8fb647a8f0e261000b0184a19ba78807b45b8969
thethinur <thinurtorsson@gmail.com>
2020-05-05 17:20:56 +0200

    Update lambda.zig
    Only accepts functions now.

--- lib/lambda.zig
+++ lib/lambda.zig
@@ -1,14 +1,17 @@
 fn createLambdaT(comptime closureT: type, comptime _func: var) type {
     return struct {
         closure: closureT,
-        pub const func = _func.func;
+        pub const func = _func;
     };
 }
 
 pub fn createLambda(closure: var, comptime _func: var) createLambdaT(@TypeOf(closure), _func) {
 
-    if (!@hasDecl(_func, "func")){
-        @compileError("'func' missing from Lambda struct Declaration");
+    const typename = @typeName(@TypeOf(_func));
+    const funcname = (typename)[0..2];
+
+    if (funcname ==  "fn"){
+        @compileError("Function missing from Lambda Declaration");
     }
     
     const LambdaT = createLambdaT(@TypeOf(closure), _func);
