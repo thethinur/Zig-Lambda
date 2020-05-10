46a871d0af2bfd0dd745816da91e956f7e559bc3
thethinur <thinurtorsson@gmail.com>
2020-05-10 22:09:50 +0200

    lambda implementation change, names match guidelines

--- lib/lambda.zig
+++ lib/lambda.zig
@@ -1,28 +1,30 @@
 const std = @import("std");
 const eql = std.mem.eql;
 
-fn createLambdaT(comptime closureT: type, comptime _func: var) type {
+fn CreateLambdaT(comptime closureT: type, comptime Struct: type) type {
     return struct {
         closure: closureT,
-        pub const func = _func;
+        pub fn call(self: @This(), args: var) Struct.ReturnType {
+            return @call(.{}, Struct.func, .{ self.closure } ++ args);
+        }
     };
 }
 
-pub fn createLambda(closure: var, comptime _func: var) createLambdaT(@TypeOf(closure), _func) {
-
-    const typename = @typeName(@TypeOf(_func));
+pub fn createLambda(closure: var, comptime Struct: var) CreateLambdaT(@TypeOf(closure), Struct) {
 
     comptime {
-        if (!eql(u8, typename[0..6] , "fn(var")){
-            @compileError("function missing from Lambda Declaration");
+        if (@hasDecl(Struct, "Func")){
+            @compileError("function: 'Func' missing from Lambda Declaration");
+        }
+        if (@hasDecl(Struct, "ReturnType")){
+            @compileError("type: ReturnType missing from Lambda Declaration");
         }
         if (@typeInfo(@TypeOf(closure)) != .Struct) {
             @compileError("closure is not a struct");
         }
     }
-  
     
-    const LambdaT = createLambdaT(@TypeOf(closure), _func);
+    const LambdaT = CreateLambdaT(@TypeOf(closure),  Struct);
 
     return LambdaT { .closure = closure };
 }
