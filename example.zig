b76b5ce4dcacf8604617b60e9e145575481c0425
thethinur <thinurtorsson@gmail.com>
2022-10-24 23:17:09 +0200

    update example to new format

--- example.zig
+++ example.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const assert = std.debug.assert;
 
 const lambda = @import("lambda/lambda.zig");
-const createLambda = lambda.createLambda;
+const lambda = lambda.lambda;
 
 pub fn main() !void
 {
@@ -11,16 +11,15 @@ pub fn main() !void
     var a: i32 = 10;
     var b: i32 = 20;
 
-    var lambda1 = createLambda(
+    var lambda1 = lambda(
         .{ .a = a, .b = &b, }, 
     struct {
-        pub const ReturnType = i32;
-        pub fn func(closure: var) ReturnType 
+        pub fn func(closure: var) i32 
         {  
 
             return closure.a + closure.b.*; 
         }
-    }
+    }.func
     );
 
     try stdout.print("{}\n", .{ lambda1.call(.{}) });
@@ -30,16 +29,15 @@ pub fn main() !void
 
     try stdout.print("{}\n", .{ lambda1.call(.{}) });
 
-    var lambda2 = createLambda(
+    var lambda2 = lambda(
         .{ .b = &b, }, 
     struct {
-        pub const ReturnType = i32;
-        pub fn func(closure: var, a2: i32) ReturnType 
+        pub fn func(closure: var, a2: i32) i32 
         { 
 
             return a2 + closure.b.*; 
         }
-    }
+    }.func
     );
 
     try stdout.print("{}\n", .{ lambda2.call(.{40}) });
